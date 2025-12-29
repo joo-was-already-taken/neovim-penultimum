63f2c338f4caec222e7477eb92bc5288a3389e47
joo-was-already-taken <trackpointus@protonmail.com>
2025-12-30 00:29:08 +0100

    package home-manager module with flake.nix

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
             neovim
             tree-sitter
             lua-language-server
+            stylua
             nodejs
             stdenv
             ripgrep
@@ -41,10 +42,13 @@
     ) // rec {
       homeManagerModules.default = homeManagerModules.neovim-penultimum;
       homeManagerModules.neovim-penultimum = { config, lib, pkgs, ... }: let
+        cfg = config.programs.neovim-penultimum;
         patchedConfig = pkgs.runCommand "neovim-patched-config" {} ''
           mkdir -p $out
           cp -r ${self}/lua $out/
-          sed 's/vim.g.auto_download = true/vim.g.auto_download = false/' \
+          sed \
+            -e 's/vim.g.allow_downloads =.*/vim.g.allow_downloads = false/' \
+            -e 's/vim.g.default_colorscheme =.*/vim.g.default_colorscheme = "${cfg.colorscheme}"/' \
             ${self}/init.lua > $out/init.lua
         '';
         nvim-tmux-navigation = pkgs.vimUtils.buildVimPlugin {
@@ -63,8 +67,60 @@
         };
 
         startPlugins = with pkgs.vimPlugins; [
+          lz-n
+          plenary-nvim
+          nvim-web-devicons
+          nui-nvim
+          hydra-nvim
+          nvim-notify
         ];
-        optionalPlugins = with pkgs.vimPlugins; [
+        optPlugins = with pkgs.vimPlugins; [
+          neo-tree-nvim
+          lualine-nvim
+          noice-nvim
+          nvim-surround
+          indent-blankline-nvim-lua
+          nvim-tmux-navigation
+          telescope-nvim
+          vim-obsession
+          smart-splits-nvim
+          blink-cmp
+          mini-pairs
+          nvim-lspconfig
+          lspsaga-nvim
+          undotree
+          conform-nvim
+
+          evergarden-colorscheme
+
+          (nvim-treesitter.withPlugins (plugins: with plugins; [
+            tree-sitter-nix
+            tree-sitter-lua
+            tree-sitter-bash
+            tree-sitter-gitignore
+            tree-sitter-python
+            tree-sitter-markdown
+            tree-sitter-markdown_inline
+            tree-sitter-typst
+            tree-sitter-latex
+            tree-sitter-make
+            tree-sitter-regex
+            tree-sitter-toml
+            tree-sitter-yaml
+            tree-sitter-json
+            tree-sitter-vim
+            tree-sitter-vimdoc
+            tree-sitter-rust
+            tree-sitter-haskell
+            tree-sitter-go
+            tree-sitter-html
+            tree-sitter-c
+            tree-sitter-cpp
+            tree-sitter-cmake
+            tree-sitter-zig
+            tree-sitter-javascript
+            tree-sitter-css
+          ]))
         ];
       in {
         options.programs.neovim-penultimum = {
@@ -74,11 +130,10 @@
             description = "Enable neovim-penultimum config";
           };
           colorscheme = lib.mkOption {
-            type = lib.types.string;
+            type = lib.types.str;
             default = "evergarden";
-            # description = ""
+            description = "Colorscheme to set via `vim.cmd.colorscheme`";
           };
-
         };
 
         config.programs.neovim = {
@@ -86,13 +141,9 @@
             wl-clipboard
             ripgrep
             fd
-
-            nil
-            tinymist
-            bash-language-server
           ];
           plugins = startPlugins
-            ++ (map (p: { plugin = p; optional = true; }) optionalPlugins);
+            ++ (map (p: { plugin = p; optional = true; }) optPlugins);
         };
 
         config.xdg.configFile."nvim".source = patchedConfig;
