404472b24cd5d97318e3c000b3e8d8f481792cc0
joo-was-already-taken <s.w.wojciechowski@proton.me>
2026-06-28 20:14:29 +0200

    fix remaining 26.05 warnings

--- nix/home-manager.nix
+++ nix/home-manager.nix
@@ -31,7 +31,7 @@
       config = lib.mkIf cfg.enable {
         programs.neovim = {
           enable = true;
-          package = inputs.neovim-nightly.packages.${pkgs.system}.default;
+          package = inputs.neovim-nightly.packages.${pkgs.stdenv.hostPlatform.system}.default;
           withRuby = false;
           withPython3 = false;
           extraPackages = with pkgs; [
