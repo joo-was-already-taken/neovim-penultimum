629ed8d93b0cf89da54fdc614ae139301c4b34cb
joo-was-already-taken <s.w.wojciechowski@proton.me>
2026-08-31 19:41:00 +0200

    fix neo-tree floating window

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -25,7 +25,29 @@ return {
         show_unloaded = true,
       },
 
-      window = { position = "current" },
+      window = {
+        position = "current",
+        mappings = {
+          ["<cr>"] = "open_safe",
+        },
+      },
+      commands = {
+        open_safe = function(state)
+          local node = state.tree:get_node()
+          if node and node.type == "file" and state.current_position == "float" then
+            local path = node.path or node:get_id()
+            require("neo-tree.ui.renderer").close(state)
+            vim.cmd.edit(vim.fn.fnameescape(path))
+          else
+            local cmds = require("neo-tree.sources." .. state.name .. ".commands")
+            if cmds and cmds.open then
+              cmds.open(state)
+            else
+              require("neo-tree.sources.common.commands").open(state)
+            end
+          end
+        end,
+      },
     })
 
     local keymap_set = require("utils").keymap_set
